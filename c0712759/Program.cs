33542018718c2be05606d5aa84e3aa490665bd61
visualstudio <visualstudio@122-PC23>
2019-01-18 20:44:09 -0500

    done

--- c0712759/Program.cs
+++ c0712759/Program.cs
@@ -10,6 +10,27 @@ namespace c0712759
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark ", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _cindy = new StudentInformation("cindy", 3.9, 187);
+        }
+        class StudentInformation
+        {
+            public string Name;
+        
+            public double GPA;
+            public int Attendence;
+            public StudentInformation(string aName, double cGPA, int dAttendence)
+            {
+                Name = aName;
+                GPA = cGPA;
+                Attendence = dAttendence;
+
+
+
+            }
         }
     }
 }
